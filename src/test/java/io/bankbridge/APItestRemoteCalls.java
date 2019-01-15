b24e967befe73a181b71ec196df840a9b0da6113
Ian David Robinson <ian.robinson@idrsc.com>
2019-01-15 21:52:41 +0100

    Adding JUnit test files

---
+++ src/test/java/io/bankbridge/APItestRemoteCalls.java
@@ -0,0 +1,25 @@
+package io.bankbridge;
+          
+import org.junit.*;
+import static org.junit.Assert.*;
+     
+import java.util.*;
+     
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+import io.bankbridge.handler.BanksRemoteCalls;
+
+public class APItestRemoteCalls {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void test() {
+        System.out.println("enter test");
+    }
+
+    
+}
